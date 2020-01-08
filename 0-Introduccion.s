f43263ce47f68ac324d92a63a4d21756acce04cf
Matias <59626616+mativironi@users.noreply.github.com>
2020-01-08 15:11:40 -0300

    Add files via upload

---
+++ 0-Introduccion.s
@@ -0,0 +1,27 @@
+@En esta introduccion vamos a aclarar algunas cosas:
+@
+@	1)El Lenguaje de programacion utilizado es ARM Assembly (No confundirse con x86 Assembly)
+
+@	2)Utilizo una Raspberry Pi zero w, con el S.O Raspbian para ejecutar los codigos en el terminal
+
+@	3)En el caso de no disponer de una Raspberry Pi, se puede configurar una maquina virtual.
+@		Link del proceso: https://azeria-labs.com/arm-lab-vm/
+
+@	4)Existe un simulador llamado ARMsim# disponible en Windows,MAC OS y Linux
+@		Link de descarga: https://webhome.cs.uvic.ca/~nigelh/ARMSim-V2.1/
+@	Exiten diferentes formas para ejecutar codigo en Assembly (Compilacion cruzada,diferentes IDE,etc)
+@	Cada uno puede usar lo que quiera o resulte mejor en base a sus posibilidades.
+
+@	5)En caso de tener alguna duda siempre se puede revisar la pagina de ayuda oficial de ARM
+@		Link: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0068b/index.html
+
+@	6) Es importante aclarar que en este repositorio solo hay ejemplos basicos con comentarios
+@	   para que puedan entender el funcionamiento de los registros,memoria,flags,stack,etc.
+
+@	   Si usted desea aprender ARM Assembly, sera necesario acudir a Libros, Manuales de ayuda,
+@	   Paginas Web (confiables), cursos online, etc.. 
+
+@ 	7) En caso de tener alguna duda,sugerencia o comentario no dude en comunicarse conmigo
+
+@ 			by Matias Vironi
+@			Github: https://github.com/mativironi/ej-asm-pi
